dfe88c03bdf431d40d207d418990a6d52b8d1d06
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-06-20 09:41:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function createPictureCard({
 }) {
   return `<div class="photo-card">
   <a href="${largeImageURL}">
-  <img src=${webformatURL} alt="${tags}" loading="lazy" />
+  <img class="photo-img" src=${webformatURL} alt="${tags}" loading="lazy" />
   </a>
   <div class="info">
     <p class="info-item">
